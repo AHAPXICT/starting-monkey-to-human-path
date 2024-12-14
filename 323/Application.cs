76b7b7f03599060bf396385ad3ccc0b63f2ef898
max manackow <max.manackow@gmail.com>
2024-12-14 21:55:09 +0300

    Added entry point

---
+++ 323/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace starting_monkey_to_hyman_path._323
+{
+    internal class Application
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm Maxim Manackow, and I'm not a monkey (I'm cat)");
+        }
+    }
+}
