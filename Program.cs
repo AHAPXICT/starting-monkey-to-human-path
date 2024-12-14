eeea5be6f109f9768301ed5f9f465c0d442db632
max manackow <max.manackow@gmail.com>
2024-12-14 21:29:14 +0300

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace starting_monkey_to_hyman_path
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
